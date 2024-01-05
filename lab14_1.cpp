e9ddfc75b0fa74404e9cffdb7b87837d886ced8a
pairrz <dararat.buayim@gmail.com>
2024-01-05 16:11:39 +0700

    01

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    
+    for(int i = 0; i < N-1; i++){;
+        if(d[i] < d[i+1]){
+            int j = i;
+            while(j >= 0){
+                if(d[j+1] < d[j]) break;
+                T temp = d[j];
+                d[j] = d[j+1];
+                d[j+1] = temp;
+                j--;
+            }
+        }
+        
+        cout << "Pass " << i+1 << ":";
+        for(int k = 0; k < N; k++) cout << d[k] <<" ";
+        cout << endl;
+    }
 }
 
 int main(){
